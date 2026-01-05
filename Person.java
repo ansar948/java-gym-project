8a50daf521e09e35ca32fdeb1541395b768210e8
ansar948 <ansar.mahmut06@gmail.com>
2026-01-05 13:35:17 +0500

    Person.java

--- Person.java
+++ Person.java
@@ -14,4 +14,9 @@ public class Person {
     public String getName() {
         return name;
     }
+
+    @Override
+    public String toString() {
+        return name + " (" + age + " years) - " + getRole();
+    }
 }
