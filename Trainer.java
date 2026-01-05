9afb5fc03c13222c9db67535761628c4b200c061
ansar948 <ansar.mahmut06@gmail.com>
2026-01-05 13:24:19 +0500

    Trainer.java

--- Trainer.java
+++ Trainer.java
@@ -1,33 +1,20 @@
-public class Trainer {
-    private String name;
+public class Trainer extends Person {
+
     private int experienceYears;
     private String specialization;
 
-    public Trainer(String name, int experienceYears, String specialization) {
-        setName(name);
-        setExperienceYears(experienceYears);
+    public Trainer(String name, int age, int experienceYears, String specialization) {
+        super(name, age);
+        this.experienceYears = experienceYears;
         this.specialization = specialization;
     }
 
-    public void setName(String name) {
-        if (name == null || name.isBlank()) {
-            throw new IllegalArgumentException("Trainer name cannot be empty");
-        }
-        this.name = name;
-    }
-
-    public void setExperienceYears(int experienceYears) {
-        if (experienceYears < 0) {
-            throw new IllegalArgumentException("Experience cannot be negative");
-        }
-        this.experienceYears = experienceYears;
-    }
-
-    public String getName() { return name; }
-
     @Override
-    public String toString() {
-        return String.format("%-10s | Experience: %-2d years | Specialization: %s",
-                name, experienceYears, specialization);
+    public String getRole() {
+        return "Trainer";
+    }
+
+    public void conductTraining(Member member) {
+        System.out.println(name + " trains " + member.getName());
     }
 }
