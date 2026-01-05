f79f8c0c624bf96cf37158d119daa63917eebff7
ansar948 <ansar.mahmut06@gmail.com>
2026-01-05 13:23:06 +0500

    Member.java

--- Member.java
+++ Member.java
@@ -1,59 +1,22 @@
-public class Member {
-    private String name;
-    private int age;
+public class Member extends Person {
+
     private double weight;
     private boolean hasMembership;
 
     public Member(String name, int age, double weight, boolean hasMembership) {
-        this.name = name;
-        this.age = age;
+        super(name, age);
         this.weight = weight;
         this.hasMembership = hasMembership;
     }
 
-    public String getName() {
-        return name;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public double getWeight() {
-        return weight;
-    }
-
-    public boolean hasMembership() {
-        return hasMembership;
-    }
-
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
-    }
-
-    public void setWeight(double weight) {
-        this.weight = weight;
-    }
-
-    public void setHasMembership(boolean hasMembership) {
-        this.hasMembership = hasMembership;
+    @Override
+    public String getRole() {
+        return "Gym Member";
     }
 
     public void train() {
         if (hasMembership) {
-            System.out.println(name + " is training.");
-        } else {
-            System.out.println(name + " has no membership.");
+            System.out.println(name + " is training");
         }
-    }
-
-    @Override
-    public String toString() {
-        return name;
     }
 }
