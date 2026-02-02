009e8e4112c97a52b4836e4fb5f0b02b7a6d4754
ansar948 <ansar.mahmut06@gmail.com>
2026-02-03 02:01:28 +0500

    Member.java

--- Member.java
+++ Member.java
@@ -1,24 +1,43 @@
-public class Member extends Person {
+package model;
 
-    private double weight;
-    private boolean hasMembership;
+public class Member extends Person implements Trainable {
+
+    private final double weight;
+    private final boolean hasMembership;
 
     public Member(String name, int age, double weight, boolean hasMembership) {
         super(name, age);
+
+        if (weight <= 0) {
+            throw new IllegalArgumentException("Weight must be positive");
+        }
+
         this.weight = weight;
         this.hasMembership = hasMembership;
     }
 
+
+
+
     @Override
     public String getRole() {
-        return "Gym Member";
+        return "Member";
     }
 
+    @Override
     public void train() {
         if (hasMembership) {
             System.out.println(name + " is training!");
         } else {
             System.out.println(name + " cannot train without membership.");
         }
+    }
+
+    public boolean hasMembership() {
+        return hasMembership;
+    }
+
+    public double getWeight() {
+        return weight;
     }
 }
