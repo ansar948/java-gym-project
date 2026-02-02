15da011c3876f36cd7e0944f50738b630262c833
ansar948 <ansar.mahmut06@gmail.com>
2026-02-03 02:03:50 +0500

    Trainer.java

--- Trainer.java
+++ Trainer.java
@@ -1,3 +1,5 @@
+package model;
+
 public class Trainer extends Person {
 
     private int experienceYears;
@@ -5,17 +7,32 @@ public class Trainer extends Person {
 
     public Trainer(String name, int age, int experienceYears, String specialization) {
         super(name, age);
+
+        if (experienceYears < 0) {
+            throw new IllegalArgumentException("Experience years must be >= 0");
+        }
+        if (specialization == null || specialization.trim().isEmpty()) {
+            throw new IllegalArgumentException("Specialization cannot be empty");
+        }
+
         this.experienceYears = experienceYears;
         this.specialization = specialization;
     }
 
     @Override
     public String getRole() {
-
-        return "Trainer";
+        return "model.Trainer";
     }
 
     public void conductTraining(Member member) {
         System.out.println(name + " trains " + member.getName());
+    }
+
+    public int getExperienceYears() {
+        return experienceYears;
+    }
+
+    public String getSpecialization() {
+        return specialization;
     }
 }
